dc7b68992d1783e5fe146715a5f49795988758c5
JianBBB <76256461+JianBBB@users.noreply.github.com>
2025-03-10 10:12:22 +0900

    [level 3] Title: 등굣길, Time: 0.87 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,29 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int MOD = 1_000_000_007; 
-        int[][] dp = new int[n + 1][m + 1]; 
-        
-
-        boolean[][] isPuddle = new boolean[n + 1][m + 1];
-        for (int[] puddle : puddles) {
-            isPuddle[puddle[1]][puddle[0]] = true; 
-        }
-        
-        dp[1][1] = 1;
-        
-        for (int y = 1; y <= n; y++) {
-            for (int x = 1; x <= m; x++) {
-                if (isPuddle[y][x]) {
-                    dp[y][x] = 0;
-                } else {
-                    if (x > 1) dp[y][x] = (dp[y][x] + dp[y][x - 1]) % MOD; 
-                    if (y > 1) dp[y][x] = (dp[y][x] + dp[y - 1][x]) % MOD; 
-                }
-            }
-        }
-        
-        return dp[n][m]; 
+        int answer = 0;
+        return answer;
     }
 }
