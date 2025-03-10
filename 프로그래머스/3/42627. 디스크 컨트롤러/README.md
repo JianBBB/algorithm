1a6b08ff1062ca590e06155127d1969b5a721e1f
JianBBB <76256461+JianBBB@users.noreply.github.com>
2025-03-10 09:34:22 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 9.44 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,6 +1,6 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 10일 09:33:23
+2025년 03월 10일 09:34:21
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,38 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, Comparator.comparingInt(j -> j[0]));
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> {
-                if (a[1] != b[1]) return a[1] - b[1]; 
-                return a[0] - b[0]; 
-            }
-        );
-
-        int time = 0; 
-        int totalTurnaroundTime = 0; 
-        int jobIndex = 0;
-        int completedJobs = 0; 
-
-        while (completedJobs < jobs.length) {
-         
-            while (jobIndex < jobs.length && jobs[jobIndex][0] <= time) {
-                pq.add(jobs[jobIndex]); 
-                jobIndex++;
-            }
-
-            if (pq.isEmpty()) { 
-                time = jobs[jobIndex][0]; 
-            } else { 
-                int[] job = pq.poll();
-                time += job[1]; 
-                totalTurnaroundTime += time - job[0]; 
-                completedJobs++;
-            }
-        }
-
-        return totalTurnaroundTime / jobs.length; 
+        int answer = 0;
+        return answer;
     }
 }
