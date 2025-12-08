95146380f446ed0497e8aa49353e118184762ba8
JianBBB <76256461+JianBBB@users.noreply.github.com>
2025-12-08 23:15:12 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 12868 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = Long.parseLong(sc.next());
+        long B = Long.parseLong(sc.next());
+        long C = Long.parseLong(sc.next());
+        System.out.println(A+B+C);
+    }
+}
