2f88f8fad807c5e9c74a9cf3e0a244ff6627c77c
JianBBB <76256461+JianBBB@users.noreply.github.com>
2026-03-18 17:51:21 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= T; i++) {
+            sb.append("*");
+            System.out.println(sb);
+        }
+
+    }
+}
